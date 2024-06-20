f037ffe56bb38ffc41d73f7abc16c832eed25019
Chand Basha Panyam <chandpanyam54@gmail.com>
2024-06-20 21:23:22 +0530

    final code
    BlogApp MERN Stack Project

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+import path from 'path'
+
+//env config
+dotenv.config();
+
+//router import
+const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogRoutes");
+
+//mongodb connection
+connectDB();
+
+//rest objecct
+const app = express();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+//routes
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogRoutes);
+//rest api
+app.use("*",function(req,res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+})
+// Port
+const PORT = process.env.PORT || 8080;
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan
+      .white
+  );
+});
